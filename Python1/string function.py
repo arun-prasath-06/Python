43053822c7cae8346e35ba659a8d802e93465163
Arun Prasath <prasatha520@gmail.com>
2025-06-23 14:33:27 +0530

    new change

---
+++ Python1/string function.py
@@ -0,0 +1,13 @@
+str='  arun prasath works in a company from march 31'
+sub='2'
+lst="prasath"
+#lst=str.split()           #splits with "," when there is a space
+#print(str.strip(""))      #removes the space(even string) on both sides and we can also use left(lstrip) and right(rstrip) aswell
+#print(str.find("company")) #finds the index of the gicen value
+#print(len(str))            #show the toatl lenght
+#print(lst)
+#print(str.isdigit())       #checks whether its true or false
+print(str.upper())          #makes uppercase
+print(str.capitalize())     #makes capitaliza the first letter
+print(str.lower())          #makes lowercase
+print(str.isspace())        #checks true or false
