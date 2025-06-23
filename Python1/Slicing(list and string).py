4918acc71576e71ac7afcfd565f4a28bea28ba7f
Arun Prasath <prasatha520@gmail.com>
2025-06-23 13:58:24 +0530

    new

---
+++ Python1/Slicing(list and string).py
@@ -0,0 +1,5 @@
+tup="Arun prasath"
+print(tup[::-1])    #reverse printing
+print(tup[1:5])     #printing the string index 2 to 4
+print(tup[0:15:2])  #printing string from index by skipping 2
+print(tup)          #print default value 
