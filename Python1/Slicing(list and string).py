63bd43c41731b4b8bb052bc66a912795c6645232
Arun Prasath <prasatha520@gmail.com>
2025-06-23 14:08:42 +0530

    new change

--- Python1/Slicing(list and string).py
+++ Python1/Slicing(list and string).py
@@ -3,3 +3,6 @@ print(tup[::-1])    #reverse printing
 print(tup[1:5])     #printing the string index 2 to 4
 print(tup[0:15:2])  #printing string from index by skipping 2
 print(tup)          #print default value 
+print(tup[4:])      #skipping first index and printing 0 to 4 index
+print(tup[:4])      #skipping last index printing 0 to 4 index
+print(tup[::4])     #printing the index by skippig 4(jumping)
