8db735992dfdeafb2bbf2e87423bd44f54d28e6d
Arun Prasath <prasatha520@gmail.com>
2025-06-23 12:10:27 +0530

    type of data

---
+++ Python1/typeofdata.py
@@ -0,0 +1,18 @@
+a=1
+b="arun" 
+c=10.5
+d=[10,20]
+e=(10,20)
+if a== 1:
+    print(type(a))
+if b=="arun":
+    print(type(b))
+if c==10.5:
+    print(type(c))
+if d==[10,20]:
+    print(type(d))
+if e==(10,20):
+    print(type(e))
+
+
+         
