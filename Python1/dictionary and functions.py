6b482dd760ab2b575ab2e9ad23396204dadef04a
Arun Prasath <prasatha520@gmail.com>
2025-06-23 18:37:45 +0530

    new dictionary

---
+++ Python1/dictionary and functions.py
@@ -0,0 +1,11 @@
+dict1={1:'one',2:'two'}
+dict2={2:'toooo',3:'Three'}
+#lst=list(dict1)
+#print(lst)
+#print(dict1.keys())     #to print the keys 
+#print(dict1.values())   #to print the values 
+#print(dict1.items())    #to print the items(tuple) 
+#dict1.update(dict2)     #updating the dictionary with new dictionary 
+#print(dict1)            
+for i in dict1:
+    print(i,dict1[i])
