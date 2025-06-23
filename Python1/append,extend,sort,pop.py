2dc6e644651f91ff15ae193ba485dbd6c3c0890a
Arun Prasath <prasatha520@gmail.com>
2025-06-23 13:20:51 +0530

    append,sort,pop,extend

--- Python1/append,extend,sort,pop.py
+++ Python1/append,extend,sort,pop.py
@@ -1,1 +1,8 @@
+lst=list("Arunprasath")
+lst.append("P")
+lst.extend("ponnusamy")
+lst.sort()
+A=lst.pop(3)
+print(lst)
+print(A)
 
---
+++ Python1/list.py
@@ -0,0 +1,3 @@
+lst=[10,20,30,60]
+print((10*2)in lst)
+print(lst[-1]+lst[1])
