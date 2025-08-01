0b393d5edddce0df9b76edb7b2c67ae2d02fde30
Arun Prasath <prasatha520@gmail.com>
2025-08-01 11:38:12 +0530

    class

---
+++ Python1/class.py
@@ -0,0 +1,10 @@
+class Bank_Account:
+    customer_name=""
+    balance = 0.0
+    account_number= 0
+
+Bank=Bank_Account()
+Bank_Account.customer_name= "Arun"
+Bank_Account.balance=500000000
+Bank_Account.account_number= 58
+print(Bank_Account.customer_name,"\n",Bank_Account.balance,"\n",Bank_Account.account_number)
